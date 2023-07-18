155168fae43870888bf7943a84788291987e1fc4
FaintSmileVL <faintsmilevl@gmail.com>
2023-07-18 22:30:15 +0600

    Initial commit

---
+++ src/ru/ICacher.java
@@ -0,0 +1,6 @@
+package ru;/**
+ * @author : faint
+ * @date : 18.07.2023
+ * @time : 22:28
+ */public interface ICacher {
+}
---
+++ src/ru/RuntimeCacher.java
@@ -0,0 +1,6 @@
+package ru;/**
+ * @author : faint
+ * @date : 18.07.2023
+ * @time : 22:29
+ */public class RuntimeCacher {
+}
