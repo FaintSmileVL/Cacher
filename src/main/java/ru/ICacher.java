cb37f1b23c1dbb323ce91b39aa49b9b3ab62940a
n1rmata <kiryl.valiushka@gmail.com>
2025-07-10 15:35:52 +0200

    fix

--- src/main/java/ru/ICacher.java
+++ src/main/java/ru/ICacher.java
@@ -1,6 +1,12 @@
-package ru;/**
+package ru;
+
+/**
  * @author : faint
  * @date : 18.07.2023
  * @time : 22:28
- */public interface ICacher {
+ */
+public interface ICacher<T> {
+    T get(String var1);
+
+    T set(String var1, T var2);
 }
--- src/main/java/ru/RuntimeCacher.java
+++ src/main/java/ru/RuntimeCacher.java
@@ -1,6 +1,21 @@
-package ru;/**
+package ru;
+
+import java.util.HashMap;
+
+/**
  * @author : faint
  * @date : 18.07.2023
  * @time : 22:29
- */public class RuntimeCacher {
+ */
+public class RuntimeCacher<T> implements ICacher<T> {
+    private final HashMap<String, T> storage = new HashMap();
+
+    public T get(String key) {
+        T result = (T) this.storage.get(key);
+        return result;
+    }
+
+    public T set(String key, T cachedObject) {
+        return (T) this.storage.putIfAbsent(key, cachedObject);
+    }
 }
